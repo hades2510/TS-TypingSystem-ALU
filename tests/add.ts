c93df1152210b716d1771fe58b24c8e5055af422
Radu Dita <hades2510@yahoo.com>
2022-10-22 22:09:47 +0300

    add ByteAdd and 2's complement to the operations

--- tests/add.ts
+++ tests/add.ts
@@ -1,8 +1,14 @@
-let carr000: AreEqual<AddOneWithCarry<Zero, Zero, Zero>, {value: Zero, cf: Zero}> = true
-let carr100: AreEqual<AddOneWithCarry<One, Zero, Zero>, {value: One, cf: Zero}> = true
-let carr110: AreEqual<AddOneWithCarry<One, One, Zero>, {value: Zero, cf: One}> = true
-let carr010: AreEqual<AddOneWithCarry<Zero, One, Zero>, {value: One, cf: Zero}> = true
-let carr001: AreEqual<AddOneWithCarry<Zero, Zero, One>, {value: One, cf: Zero}> = true
-let carr011: AreEqual<AddOneWithCarry<Zero, One, One>, {value: Zero, cf: One}> = true
-let carr101: AreEqual<AddOneWithCarry<One, Zero, One>, {value: Zero, cf: One}> = true
-let carr111: AreEqual<AddOneWithCarry<One, One, One>, {value: One, cf: One}> = true
+let carr000: AreEqual<AddBitsWithCarry<Zero, Zero, Zero>, {value: Zero, cf: Zero}> = true
+let carr100: AreEqual<AddBitsWithCarry<One, Zero, Zero>, {value: One, cf: Zero}> = true
+let carr110: AreEqual<AddBitsWithCarry<One, One, Zero>, {value: Zero, cf: One}> = true
+let carr010: AreEqual<AddBitsWithCarry<Zero, One, Zero>, {value: One, cf: Zero}> = true
+let carr001: AreEqual<AddBitsWithCarry<Zero, Zero, One>, {value: One, cf: Zero}> = true
+let carr011: AreEqual<AddBitsWithCarry<Zero, One, One>, {value: Zero, cf: One}> = true
+let carr101: AreEqual<AddBitsWithCarry<One, Zero, One>, {value: Zero, cf: One}> = true
+let carr111: AreEqual<AddBitsWithCarry<One, One, One>, {value: One, cf: One}> = true
+
+const onePlusThree: AreEqual<Add<ONE, THREE>, FOUR> = true
+const twoPlusTwo: AreEqual<Add<TWO, TWO>, FOUR> = true
+const threePlusThree: AreEqual<Add<THREE, THREE>, [Zero, Zero, Zero, Zero, Zero, One, One, Zero]> = true
+const zeroPlusZero: AreEqual<Add<ZERO_BYTE, ZERO_BYTE>, ZERO_BYTE> = true
+const fourPlusFour: AreEqual<Add<FOUR, FOUR>, ShiftLeft<ShiftLeft<ShiftLeft<ONE>>>> = true
---
+++ tests/subtract.ts
@@ -0,0 +1,2 @@
+const minus0: AreEqual<Byte2Complement<ZERO_BYTE>, ZERO_BYTE> = true
+const minus127: AreEqual<Byte2Complement<ONE>, ONLY_ONE> = true
