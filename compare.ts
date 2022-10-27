b05355d501c8beeb760b6d1079920717f2f81de8
Radu Dita <hades2510@yahoo.com>
2022-10-27 08:46:01 +0300

    add operation for GT and GTE

--- compare.ts
+++ compare.ts
@@ -6,3 +6,6 @@ type BitLTB<LHS extends Bit, RHS extends Bit> = Zero extends LHS ? One extends R
 type LTPartial<LHS extends Bit[], RHS extends Bit[]> =  LHS extends [infer LHSU extends Bit, ...infer LHSR extends Bit[]] ? RHS extends [infer RHSU extends Bit, ...infer RHSR extends Bit[]] ? true extends BitLTB<LHSU, RHSU> ? true : false extends BitLTB<LHSU, RHSU> ? false : "undecided" extends BitLTB<LHSU, RHSU> ? LTPartial<LHSR, RHSR> : false : false : false
 type LT<LHS extends Byte, RHS extends Byte> = LHS extends [infer LHSU extends Bit, ...infer LHSR extends Bit[]] ? RHS extends [infer RHSU extends Bit, ...infer RHSR extends Bit[]] ? true extends BitLTB<LHSU, RHSU> ? true : false extends BitLTB<LHSU, RHSU> ? false : "undecided" extends BitLTB<LHSU, RHSU> ? LTPartial<LHSR, RHSR> : never : never : never
 type LTE<LHS extends Byte, RHS extends Byte> = true extends LT<LHS, RHS> ? true : EQ<LHS, RHS>
+
+type GT<LHS extends Byte, RHS extends Byte> = LT<RHS, LHS>
+type GTE<LHS extends Byte, RHS extends Byte> = LTE<RHS, LHS>
