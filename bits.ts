28d9176059ac6e53922d5b7e1ba82a7a16330d88
Radu Dita <hades2510@yahoo.com>
2022-10-27 08:49:36 +0300

    remove main and split it in bits and byte

--- bits.ts
+++ bits.ts
@@ -5,3 +5,10 @@ type Zero = {
 type One = {
     __type: 'one'
 }
+
+type Bit = Zero | One;
+
+type BitNot <T extends Bit> = T extends Zero ? One : Zero;
+type BitAnd <LHS extends Bit, RHS extends Bit> = LHS extends One ? RHS extends One ? One : Zero : Zero;
+type BitOr <LHS extends Bit, RHS extends Bit> = LHS extends Zero ? RHS extends Zero ? Zero : One : One;
+type BitXor <LHS extends Bit, RHS extends Bit> = LHS extends Zero ? RHS extends One ? One : Zero : RHS extends Zero ? One : Zero;
--- byte.ts
+++ byte.ts
@@ -1,11 +1,5 @@
-type Bit = Zero | One;
 
 type Byte = [Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit];
-
-type BitNot <T extends Bit> = T extends Zero ? One : Zero;
-type BitAnd <LHS extends Bit, RHS extends Bit> = LHS extends One ? RHS extends One ? One : Zero : Zero;
-type BitOr <LHS extends Bit, RHS extends Bit> = LHS extends Zero ? RHS extends Zero ? Zero : One : One;
-type BitXor <LHS extends Bit, RHS extends Bit> = LHS extends Zero ? RHS extends One ? One : Zero : RHS extends Zero ? One : Zero;
 
 type PartialByteNot<T extends Bit[]>  = T extends [infer U extends Bit, ...infer R extends Bit[]] ? [BitNot<U>, ...PartialByteNot<R>] : []
 type ByteNot <T extends Byte> = T extends [infer U extends Bit, ...infer R extends Bit[]] ? [BitNot<U>, ...PartialByteNot<R>] : []
