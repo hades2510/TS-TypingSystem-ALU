3359fa4852d3f7c09d6a0aa32bf4314efce34ff3
Radu Dita <hades2510@yahoo.com>
2022-10-27 06:51:45 +0300

    add LT operation

--- compare.ts
+++ compare.ts
@@ -1,1 +1,7 @@
 type EQ<LHS extends Byte, RHS extends Byte> = ZERO_BYTE extends ByteXOR<LHS, RHS> ? true : false
+
+//                                                                                   01      00                               11           10
+type BitLTB<LHS extends Bit, RHS extends Bit> = Zero extends LHS ? One extends RHS ? true : "undecided" : One extends RHS ? "undecided" : false
+
+type LTPartial<LHS extends Bit[], RHS extends Bit[]> =  LHS extends [infer LHSU extends Bit, ...infer LHSR extends Bit[]] ? RHS extends [infer RHSU extends Bit, ...infer RHSR extends Bit[]] ? true extends BitLTB<LHSU, RHSU> ? true : false extends BitLTB<LHSU, RHSU> ? false : "undecided" extends BitLTB<LHSU, RHSU> ? LTPartial<LHSR, RHSR> : false : false : false
+type LT<LHS extends Byte, RHS extends Byte> = LHS extends [infer LHSU extends Bit, ...infer LHSR extends Bit[]] ? RHS extends [infer RHSU extends Bit, ...infer RHSR extends Bit[]] ? true extends BitLTB<LHSU, RHSU> ? true : false extends BitLTB<LHSU, RHSU> ? false : "undecided" extends BitLTB<LHSU, RHSU> ? LTPartial<LHSR, RHSR> : never : never : never
--- tests/compare.ts
+++ tests/compare.ts
@@ -2,3 +2,17 @@ const eq1_1: AreEqual<EQ<ONE_BYTE, ONE_BYTE>, true> = true
 const eq1_0: AreEqual<EQ<ONE_BYTE, ZERO_BYTE>, false> = true
 const eq0_0: AreEqual<EQ<ZERO_BYTE, ZERO_BYTE>, true> = true
 const eq_2_4: AreEqual<EQ<TWO, FOUR>, false> = true
+
+const bitltb1_1: AreEqual<BitLTB<Zero, Zero>, "undecided"> = true
+const bitltb1_0: AreEqual<BitLTB<One, Zero>, false> = true
+const bitltb0_0: AreEqual<BitLTB<Zero, Zero>, "undecided"> = true
+const bitltb0_1: AreEqual<BitLTB<Zero, One>, true> = true
+
+const bitlt0_1: AreEqual<LT<ZERO_BYTE, ONE_BYTE>, true> = true
+const bitlt1_0: AreEqual<LT<ONE_BYTE, ZERO_BYTE>, false> = true
+const bitlt2_0: AreEqual<LT<TWO, ZERO_BYTE>, false> = true
+const bitlt2_4: AreEqual<LT<TWO, FOUR>, true> = true
+const bitlt2_2: AreEqual<LT<TWO, TWO>, false> = true
+const bitlt1_1: AreEqual<LT<ONE_BYTE, ONE_BYTE>, false> = true
+const bitlt4_0: AreEqual<LT<FOUR, ZERO_BYTE>, false> = true
+const bitlt0_4: AreEqual<LT<ZERO_BYTE, FOUR>, true> = true
